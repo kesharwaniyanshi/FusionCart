7c771af68d2308d616d879f0c8f48a3d1bb02fea
kesharwaniyanshi <kesharwaniyanshi@gmail.com>
2024-10-23 18:41:50 +0530

    commit

--- fusioncart-frontend/src/components/home/Home.jsx
+++ fusioncart-frontend/src/components/home/Home.jsx
@@ -1,9 +1,22 @@
+import { Box, styled } from "@mui/material";
+import Banner from "./banner";
 import NavBar from "./NavBar";
+
+const Component=styled(Box)`
+margin-top: 120px;
+padding: 10px 10px;
+background: #F8F8F8;
+${'' /* background: RGB(43,99,86); */}
+`;
 
 const Home = () => {
     return (
-
-        <NavBar/>
+        <>
+            <NavBar />
+            <Component>
+            <Banner/>
+            </Component>
+        </>
     );
 };
 export default Home;
---
+++ fusioncart-frontend/src/components/home/banner.jsx
@@ -0,0 +1,53 @@
+import Carousel from "react-multi-carousel";
+import 'react-multi-carousel/lib/styles.css';
+import { bannerData } from "../../constants/data";
+import { styled } from "@mui/material";
+
+const Image = styled('img')({
+    width: '100%',
+    height: 280,
+});
+
+const responsive = {
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 1
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 1
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1
+    }
+};
+
+// Add a styled div with margin-top
+// const CarouselContainer = styled('div')({
+//     marginTop: '120px', // Shift the carousel 120px from the top
+// });
+
+const Banner = () => {
+    return (
+        
+            <Carousel
+                swipeable={false}
+                draggable={false}
+                infinite={true}
+                autoPlay={true}
+                autoPlaySpeed={4000}
+                containerClass="carousel-container"
+                dotListClass="custom-dot-list-style"
+                itemClass="carousel-item-padding-40-px"
+                responsive={responsive}>
+                {
+                    bannerData.map(data => (
+                        <Image src={data.url} alt="banner" key={data.url} />
+                    ))
+                }
+            </Carousel>
+        
+    )
+}
+export default Banner;
