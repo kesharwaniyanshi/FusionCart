91e8bfe1507df8b3d400a0612d450f4297ff7b57
kesharwaniyanshi <kesharwaniyanshi@gmail.com>
2024-10-24 15:05:21 +0530

    commit

---
+++ fusioncart-frontend/src/components/login/loginDialog.jsx
@@ -0,0 +1,11 @@
+import { Dialog } from "@mui/material";
+
+const LoginDialog = () => {
+    return (
+        <Dialog open={true}>
+            Hello
+        </Dialog>
+    )
+};
+
+export default LoginDialog;
