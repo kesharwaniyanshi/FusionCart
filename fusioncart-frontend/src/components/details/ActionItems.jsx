b28922c7c84b755f7bac13cbfb7398bb38e3027e
kesharwaniyanshi <kesharwaniyanshi@gmail.com>
2024-11-02 15:02:05 +0530

    commit

---
+++ fusioncart-frontend/src/components/details/ActionItems.jsx
@@ -0,0 +1,11 @@
+
+
+
+const ActionItem=() =>{
+return (
+    <div>
+        Image
+    </div>
+)
+}
+export default ActionItem;
--- fusioncart-frontend/src/components/details/DetailView.jsx
+++ fusioncart-frontend/src/components/details/DetailView.jsx
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getProductDetails } from "../../redux/actions/productActions";
+import { Box, Typography } from "@mui/material";
+import ActionItem from "./ActionItems";
 
 
 const DetailView = () => {
@@ -14,14 +16,28 @@ const DetailView = () => {
     // }, [dispatch, id.product_id ]);
     const { product_id } = useParams(); // Destructure product_id
 
+    const [loading, product] = useSelector(state => state.getProductDetails);
     useEffect(() => {
-        dispatch(getProductDetails(product_id));
-    }, [dispatch, product_id]);
+        if (product && product_id !== product.product_id)
+            dispatch(getProductDetails(product_id));
+    }, [dispatch, product_id, product, loading]);
 
+    console.log(product);
     return (
-        <div>
-            hi
-        </div>
+        <Box>
+            {
+                product && Object.keys(product).length &&
+                <Box>
+                    <Box>       
+                    <ActionItem product={product}/>
+                    </Box>
+                    <Box>
+                        <Typography>
+                            {product.product_name}
+                        </Typography>                </Box>
+                </Box>
+            }
+        </Box>
     )
 }
 export default DetailView;
